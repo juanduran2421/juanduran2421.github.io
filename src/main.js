c20d3a6ae87e6ee1bb07b307dfa2113a13975953
Juan Esteban Duran <jduran@truora.com>
2023-09-18 22:01:33 -0500

    Create hello world proyect

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import './index.css'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
-
-createApp(App)
-  .use(router)
-  .use(store)
-  .mount('#app')
+createApp(App).mount('#app')
